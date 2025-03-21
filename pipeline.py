1bcea4c0ca0c2a5dbab3bf53b4820c2485f27acb
Asad <muhammadasad98980@gmail.com>
2025-03-21 09:20:44 +0500

    pipeline code updated

--- pipeline.py
+++ pipeline.py
@@ -1,15 +1,16 @@
 from scrapper import *
+import sys
 
+if __name__ == "__main__":
+    brandName = sys.argv[1]
 
-brandName = 'alkaram'
+    # Step 1: Scrap all the products for the given brand
+    productsFile = scrapBrand(brandName)
+    # productsFile = 'data/data_alkaram_2025-03-11.json'
+    # print('Product File Name: ' + productsFile)
 
-# Step 1: Scrap all the products for the given brand
-# productsFile = scrapBrand(brandName)
-productsFile = 'data/data_alkaram_2025-03-11.json'
-print('Product File Name: ' + productsFile)
+    # Step 2: Remove duplicate products
+    removeDuplicates(productsFile)
 
-# Step 2: Remove duplicate products
-removeDuplicates(productsFile)
-
-# Step 3: Sort products into different categories
-categoriseProducts(brandName, productsFile)
+    # Step 3: Sort products into different categories
+    categoriseProducts(brandName, productsFile)
